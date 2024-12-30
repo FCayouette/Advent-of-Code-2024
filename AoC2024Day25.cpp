3c87c6948cc37dec29e0b003ceda005165e1519f
Francois Cayouette <45644908+FCayouette@users.noreply.github.com>
2024-12-30 14:27:29 -0500

    Simplified Day25 C++20 Solution

--- AoC2024Day25.cpp
+++ AoC2024Day25.cpp
@@ -21,49 +21,21 @@ int main(int argc, char* argv[])
 	std::vector<Pin> locks, keys;
 	while (in >> line)
 	{
-		if (line.empty()) break;
 		bool isLock = line.find('#') != std::string::npos;
-		std::vector<std::string> pattern = {std::move(line)};
+		Pin current = (isLock ? Pin{0, 0, 0, 0, 0} : Pin{-1,-1,-1,-1,-1});
 		for (int i = 0; i < 6; ++i)
 		{
 			in >> line;
-			pattern.push_back(std::move(line));
+			for (int j = 0; j < 5; ++j)
+				current[j] += line[j] == '#';
 		}
-		
-		if (Pin p; isLock)
-		{
-			for (int x = 0; x < 5; ++x)
-				for (int i = 1; i < 7; ++i)
-					if (pattern[i][x] == '.')
-					{
-						p[x] = i-1;
-						break;
-					}
-			locks.push_back(std::move(p));
-		}
-		else
-		{
-			for (int x = 0; x < 5; ++x)
-				for (int i = 5; i >= 0; --i)
-					if (pattern[i][x] == '.')
-					{
-						p[x] = 5-i;
-						break;
-					}
-			keys.push_back(std::move(p));
-		}
+		(isLock ? locks : keys).push_back(std::move(current));
 	}
 
 	int answer = 0;
 	for (const auto& l : locks)
 		for (const auto& k : keys)
-		{
-			bool ok = true;
-			for (int i = 0; ok && i < 5; ++i)
-				if (l[i]+k[i]>5)
-					ok = false;
-			answer += ok;
-		}
+			answer += std::inner_product(l.cbegin(), l.cend(), k.cbegin(), true, std::bit_and(), [](int a, int b) { return a + b < 6;});
 
 	std::cout << std::format("Answer: {}\n", answer);
 	std::cout << std::format("Duration: {}\n", std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - ChronoStart));
