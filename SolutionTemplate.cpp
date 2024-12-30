59e1256f40f8f9c13f3beedfd8a6c5f4c94b3190
Francois Cayouette <45644908+FCayouette@users.noreply.github.com>
2024-12-30 14:28:02 -0500

    Base solution template

---
+++ SolutionTemplate.cpp
@@ -0,0 +1,63 @@
+import std.core;
+
+#define ALL(x) (x).begin(),(x).end()
+#define ALLc(x) (x).cbegin(),(x).cend()
+#define ALLr(x) (x).rbegin(),(x).rend()
+#define ALLcr(x) (x).crbegin(),(x).crend()
+
+using u64 = unsigned long long;
+using i64 = long long;
+
+std::pair<std::string, std::string> SplitAtFirst(std::string s, std::string token)
+{
+	auto pos = s.find(token);
+	if (pos == std::string::npos)
+		return std::make_pair(std::move(s), std::string{});
+	return std::make_pair(s.substr(0, pos), s.substr(pos + token.size()));
+}
+
+std::vector<std::string> SplitAt(std::string s, std::string token)
+{
+	std::vector<std::string> results;
+	auto pos = s.find(token), tSize = token.size();
+	while (pos != std::string::npos)
+	{
+		results.push_back(s.substr(0, pos));
+		s = s.substr(std::min(pos + tSize, s.length()));
+		pos = s.find(token);
+	}
+	if (!s.empty())
+		results.push_back(std::move(s));
+	return results;
+}
+
+int main(int argc, char* argv[])
+{
+	auto ChronoStart = std::chrono::high_resolution_clock::now();
+	if (argc < 2)
+	{
+		std::cout << "Usage: AoC2024DayXX.exe inputFilename\n";
+		return -1;
+	}
+	std::ifstream in(argv[1]);
+	if (!in)
+	{
+		std::cout << std::format("Could not open inputFilename {}\n", argv[1]);
+		return -1;
+	}
+
+	u64 part1 = 0, part2 = 0;
+	std::string line;
+
+	//std::getline(in, line);
+	//u64 x = stoull(line);
+	//sscanf_s(line.c_str(), "%d-%d,%d-%d", &x, &X, &y, &Y);
+	while (in >> line)
+	{
+
+	}
+
+
+	std::cout << std::format("Part 1: {}\nPart 2: {}\n", part1, part2);
+	std::cout << std::format("Duration: {}\n", std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - ChronoStart));
+}
